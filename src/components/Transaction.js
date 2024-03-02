3dfc7948c8980045cbe81910445cc5bdb06dbb39
unknown <aaron.hickman201@gmail.com>
2024-03-02 23:51:48 +0000

    fixed positive and negative styles

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,7 +3,8 @@ import React from "react";
 const Transaction = ({ item }) => {
   const sign = item.amount < 0 ? "-" : "+";
   return (
-    <li className={item.value < 0 ? "minus" : "positive"}>
+    <li className={item.amount < 0 ? "minus" : "plus"}>
+      {console.log(item.amount)}
       {item.text}
       <span>
         {sign}${Math.abs(item.amount)}
